df8a1140e5cce90528f5c9b583674a7129a6b55d
Hylton <goregehylton@yahoo.com>
2024-06-10 17:21:09 -0500

    Parsing Return Statements

--- ast/ast.go
+++ ast/ast.go
@@ -36,6 +36,17 @@ func (ls *LetStatement) statementNode() {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal}
 // ********* END OF LET STATEMENT ***************
 
+// ********* RETURN STATEMENT ***************
+type ReturnStatement struct {
+	Token token.Token
+ 	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode() {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal}
+
+
+// ********* PROGRAM STRUCT ***************
 type Program struct {
 	Statements []Statement
 }
